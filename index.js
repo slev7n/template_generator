88e073b55c700350b8f57bbbe748466d67b9840c
slev7n <slev7n@gmail.com>
2019-03-08 15:04:54 +0100

    dm

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ fs.existsSync(imgdir)  || fs.mkdirSync(imgdir);
 
 let html = '<!DOCTYPE html>\n<html>\n<head>\n<title></title>\n<meta charset="UTF-8">\n<link rel="stylesheet" href="css/style.css">\n</head>\n<body>\n<script src="js/script.js"></script>\n</body>\n</html>',
 	js   = '',
-	css  = '';
+	css  = '',
+	package = '{\n\t"scripts": {\n\t\t"start": "browser-sync start --server --port 777 --directory --files \'**/*.html, **/*.css, **/*.js\'"\n\t}\n}';
 
 createFile(maindir, 'index.html', html);
+createFile(maindir, 'package.json', package);
 createFile(jsdir, 'script.js', js);
 createFile(cssdir, 'style.css', css);
 
@@ -26,4 +28,3 @@ function createFile(dir, name, content) {
 			console.log(name, 'created..');
 	});
 }
-
